5472746c925f58f346aca2db6d1299034c4498f1
Nathalia <indigodreamer11@hotmail.com>
2019-09-08 15:17:04 -0500

    zzZZ

--- src/App.js
+++ src/App.js
@@ -5,19 +5,15 @@ import Header from './components/header/Header'
 import Index from './pages/Index'
 import Estimar from './pages/Estimar'
 import Reserva from './pages/Reservar'
-import CssBaseline from '@material-ui/core/CssBaseline'
-
 import './App.css'
 
 const App = () => (
-  <CssBaseline>
     <Router>
       <Header />
       <Route path="/" exact component={Index} />
       <Route path="/estimar" component={Estimar} />
       <Route path="/reservar" component={Reserva} />
     </Router>
-  </CssBaseline>
 )
 
 export default App;
